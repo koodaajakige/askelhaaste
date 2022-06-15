d153633bc7ede4dce64437a4a1d7180144f89c14
koodaajakige <kirsi.nykanen@edu.sasky.fi>
2022-06-15 15:05:20 +0300

    Testdata added

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -3,14 +3,34 @@ import styles from './item.module.scss';
 import { MdPlayArrow } from 'react-icons/md';
 
 function Item(props) {
-    return(
-      <div className={styles.item}>
-        <div className={styles.item_data}>
-            <div className={styles.item_name}>KERTTU</div>
-            <div className={styles.item_steps}>17850</div>
-            <div className={styles.item_date}>1.6.2022</div>
-            <div className={styles.item_timespan}>28.5.2022 - 31.5.2022</div>
-        </div>
+
+  const locale = "fi-Fi";
+  const today = new Date(props.data.today).toLocaleDateString(locale);
+
+  let average;
+  let period;
+  if (props.data.periodStart && props.data.periodEnd) {
+    const periodStart = new Date(props.data.periodStart);
+    const periodEnd = new Date(props.data.periodEnd);
+    period = periodStart.toLocaleDateString(locale) + " - " + periodEnd.toLocaleDateString(locale);
+    const days = ((periodEnd - periodStart) / (1000*60*60*24))+1;
+    average = parseInt(props.data.steps / days);
+  }
+
+  else{
+    average = props.data.steps;
+  }
+
+  return(
+    <div className={styles.item}>
+      <div className={styles.item_data}>
+          <div className={styles.item_name}>{props.data.name}</div>
+          <div className={styles.item_steps}>{props.data.steps}</div>
+          <div className={styles.item_date}>{today}</div>
+          <div className={styles.item_timespan}>{props.data.periodStart}-{props.data.periodEnd}</div>
+          <div></div>
+          <div className={styles.item_average}>{average?  average + " askelta/pv" : ""}</div>
+      </div>
         <div className={styles.item_edit}>
           <div><MdPlayArrow /></div>
         </div>
---
+++ src/testdata.js
@@ -0,0 +1,49 @@
+const data = [
+  {
+    id:             "1",
+    name:           "Kerttu",
+    steps:          28520,
+    today:          "2020-11-19",
+    periodStart:    "2020-10-16",
+    periodEnd:      "2020-10-18",
+    average:        7000,
+  },
+  {
+    id:             "2",
+    name:           "Arttu",
+    steps:          8530,
+    today:          "2020-11-17",
+    periodStart:    "",
+    periodEnd:      "",
+    average:        7000,
+  },
+  {
+    id:             "3",
+    name:           "Ville",
+    steps:          14800,
+    today:          "2020-10-29",
+    periodStart:    "2020-10-28",
+    periodEnd:      "2020-10-28",
+    average:        7000,
+  },
+  {
+    id:             "4",
+    name:           "Paula",
+    steps:          7500,
+    today:          "2020-10-16",
+    periodStart:    "",
+    periodEnd:      "",
+    average:        7000,
+  },
+  {
+    id:             "5",
+    name:           "Valtteri",
+    steps:          80560,
+    today:          "2020-09-11",
+    periodStart:    "2020-09-01",
+    periodEnd:      "2020-09-10",
+    average:        7000,
+  },
+]; 
+
+export default data;
